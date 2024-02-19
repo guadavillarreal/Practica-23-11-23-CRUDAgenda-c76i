420af0f04b4c5095e4dd78f7201bfd03c23b888b
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-02-18 21:33:26 -0300

    se crean funciones crearFila y cargaInicial para que la tabla se actualice con los datos que se van cargando y guardarEnelLocalstorage para que guarde los datos ingresados

--- js/app.js
+++ js/app.js
@@ -23,19 +23,21 @@ import Contacto from "./classContacto.js";
 
 /*al borrar los -data-bs-toggle y data-bs-target- para llamar a la ventana modal desde JS elijo el metodo que deseo-
 la cual al traer el maquetado de bootstrap lo tengo que seleccionar como prototipo de bootstrap y luego ya lo selecciono de la manera que vimos-elegimos*/
-const modalAdminContacto = new bootstrap.Modal(document.getElementById('administrarContacto'));
+const modalAdminContacto = new bootstrap.Modal(
+  document.getElementById("administrarContacto")
+);
 /*tambien traigo el btnNuevoContacto,para que se abra la ventana una vez que el us haga click en el btn nuevo contacto */
-const btnAgregarContacto = document.getElementById('btnNuevoContacto');
+const btnAgregarContacto = document.getElementById("btnNuevoContacto");
 //verifico que este correcto
 // console.log(btnAgregarContacto);
 //traigo el form para asi poder crear el evento de submi al presionar el btn agragar
-const formularioContacto = document.querySelector('form');
+const formularioContacto = document.querySelector("form");
 //console.log(formularioContacto);
 //me traigo los input que necesito gurar para crear el nuevo contacto
-const nombre = document.getElementById('nombre');
-const apellido = document.getElementById('apellido');
-const email = document.getElementById('email');
-const telefono = document.getElementById('telefono');
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const telefono = document.getElementById("telefono");
 //otra forma de crear las variables podria ser, es decir sis ";" y solo "," lo que tomaria que las var q voy declarando seguido son del mismo tipo
 /*const nombre = document.getElementById('nombre'),
 apellido = document.getElementById('apellido'),*/
@@ -44,51 +46,90 @@ apellido = document.getElementById('apellido'),*/
 xlo que tengo que traerlos datos con getItem con el identificador "key" qle asignamos(agendaKey) y me devuelve -si las encuentra- en formato JSON por loq 
 lo tengo que convertirlo a loq era anteriormente- con parse-*/
 //46: trae los datos y convertilos en lo que eran O si no tiene datos inicializalo vacio
-const agenda = JSON.parse(localStorage.getItem('agendaKey')) || [ ] ;
+const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
 
 /*FUNCIONES */
 
 //funcion en flecha
-const mostrarModal = ()=>{
-    /*L:25-- modalAdminContacto.show();-- hace que se abra la ventana automaticamente al cargar la pag*/
-    modalAdminContacto.show();
-}
-const crearContacto =(e)=>{
-    /*agrego cmo param event-SIEMPRE SE USA CON EL FORM- para que no se refresque la pestaña y pueda recolectar los datos del form al realizar el evento submi */
-    e.preventDefault();
-    console.log('aqui debo crear el contacto nuevo');
-    //verificar que los datos sean validos
-    //creo un nuevo contacto
-    const nuevoContacto = new Contacto (undefined, nombre.value, apellido.value, email.value, telefono.value);
-    //console.log(nuevoContacto);
-    //guardo el contacto creado en la sig posicion del array
-    agenda.push(nuevoContacto);
-    console.log(agenda);
-    //resetear el formulario(es un metodo dele form)
-    limpiarFormulario();
-    //guarda el el localStorage
-    guardarEneLocalstorage();
-}
+const mostrarModal = () => {
+  /*L:25-- modalAdminContacto.show();-- hace que se abra la ventana automaticamente al cargar la pag*/
+  modalAdminContacto.show();
+};
+const crearContacto = (e) => {
+  /*agrego cmo param event-SIEMPRE SE USA CON EL FORM- para que no se refresque la pestaña y pueda recolectar los datos del form al realizar el evento submi */
+  e.preventDefault();
+  console.log("aqui debo crear el contacto nuevo");
+  //verificar que los datos sean validos
+  //creo un nuevo contacto
+  const nuevoContacto = new Contacto(
+    undefined,
+    nombre.value,
+    apellido.value,
+    email.value,
+    telefono.value
+  );
+  //console.log(nuevoContacto);
+  //guardo el contacto creado en la sig posicion del array
+  agenda.push(nuevoContacto);
+  console.log(agenda);
+  //resetear el formulario(es un metodo dele form)
+  limpiarFormulario();
+  //guarda el el localStorage
+  guardarEneLocalstorage();
+};
 //en este ej se desea crear func pequeñas que hagan cada una una actividad,por lo que en otra func creamos la func para limpiar el form
-//funcion 
-function limpiarFormulario (){
-    formularioContacto.reset();
+//funcion
+function limpiarFormulario() {
+  formularioContacto.reset();
 }
 /*WEB STORAGE: tiene dos obj localStrorage y sesionStorage, ambos tienen los mismos metodos:
 -setItem: guarda
 -getItem: cuando quiero obtener los datos guardados*/
 //creo una func para borrar el localStrorage
 function guardarEneLocalstorage() {
-    //para trabajar con el local Storage tengo un obj llamado localStorage
-    //setItem metodo para guardar; nos piede ima key-palabra clave-un string, con la cual queda guardada, y el dato que deseo guardar en formato JSON-notacion de obj de JS-
-    //JSON.stringify, indica que todo lo que le pase lo convierte en form JSON
-    localStorage.setItem('agendaKey',JSON.stringify(agenda) );
-    /*si las prop de la clase, obj, etc; a la que estoy accediendo no me va a permitir verlas amenos que agrege una linea de codigo extra en el arch que xport
+  //para trabajar con el local Storage tengo un obj llamado localStorage
+  //setItem metodo para guardar; nos piede ima key-palabra clave-un string, con la cual queda guardada, y el dato que deseo guardar en formato JSON-notacion de obj de JS-
+  //JSON.stringify, indica que todo lo que le pase lo convierte en form JSON
+  localStorage.setItem("agendaKey", JSON.stringify(agenda));
+  /*si las prop de la clase, obj, etc; a la que estoy accediendo no me va a permitir verlas amenos que agrege una linea de codigo extra en el arch que xport
     linea 43 en arch classContacto.js */
+}
+//creo una func(declarativa xej) para cargar los datos guardados en la agenda en la tabla del HTML
+//solo sabe crear una fila
+//paso como param -contacto- para poder completarla con los param del obj pero tambien lo tengo que pasar x param en la func que realiza la carga inicial en la func que dibuja la tabla
+//fila, creo un param para poder darle un valor consecutivo al id de c/fila
+function crearFila(contacto, fila) {
+  const tablaContactos = document.querySelector("tbody");
+  /*utilizo el innerHTML para evitar muchas lineas de codigo-q generaria el document.createHTML
+    y uso el += para concatenar lo que ya tiene mas loq ingreso */
+  tablaContactos.innerHTML += `
+    <th scope="row">${fila}</th>
+    <td>${contacto.nombre}</td>
+    <td>${contacto.apellido}</td>
+    <td>${contacto.email}</td>
+    <td>${contacto.celular}</td>
+    <td>
+      <button class="btn btn-warning">Editar</button>
+      <button class="btn btn-danger">Borrar</button>
+    </td>
+  </tr>`;
+}
+//creo una func para dibujar la tabla solo cuando hay datos
+//mapea mi agenda y si hay elem en la agenda llama a crearFila
+function cargaInicial() {
+    //si la agenda tiene datos-es decir es mayor a cero- dibujo la agenda
+    if(agenda.length > 0){
+        //map representa xpantalla algo con cada elem del array-
+        //se pasa como param -contacto- pq cargue loq crea el el us al completar el fomr-L100 func crearFila
+        //map solo suele utilizar un solo param,porq el segundo representa la posicion del elem en el array, lo que utilizo para el id de la fila, y como el array empieza en 0 debo agregarle un +1 para que empice en 1
+        agenda.map((contacto, posicion)=> crearFila(contacto, posicion+1));
+    }
 }
 
 /*LOGICA DEL CODIGO */
 
 //agrergo el manejador de evento-que hara cuando lo precionen- con el evento que se tiene que realizar y la func que se v a aejecutar
-btnAgregarContacto.addEventListener('click', mostrarModal);
-formularioContacto.addEventListener('submit', crearContacto);
+btnAgregarContacto.addEventListener("click", mostrarModal);
+formularioContacto.addEventListener("submit", crearContacto);
+
+cargaInicial();
